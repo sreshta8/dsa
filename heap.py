cd8d954eed02460faad15ec8d9d5ac5053977afc
sreshta8 <169362629+sreshta8@users.noreply.github.com>
2024-09-29 16:34:06 +0530

    Update heap.py

--- heap.py
+++ heap.py
@@ -1,1 +1,9 @@
-def sa:
+import heapq
+heap=[]
+heapq.heappush(heap,10)
+heap
+heapq.heappush(heap,1)
+heapq.heappush(heap,15)
+heapq.heappop(heap) /*deletes min value as it  by default follows min heap*/
+list1[1,6,8,9,11,7]
+heapq.heapify(list1)   
